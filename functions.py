f855cccbcbf6c96c4ba735d1e25c47edda39a715
Esai Morales <esaimn96@gmail.com>
2018-02-13 15:18:13 -0800

    ridge grad descent

--- functions.py
+++ functions.py
@@ -1,15 +1,15 @@
 import numpy as np
 import random
 
+# debugging function that prints matrix shape
 def print_shape(M):
-    # print 'Matrix Height: ', M.shape[0]
-    # print 'Matrix Widht: ', M.shape[1]
     print '(', M.shape[0], 'x', M.shape[1], ')'
 
 
 # returns w (weight-value)
 def weight(X, Y):
     a = np.dot(X.T, X)
+
     # take inverse
     a = np.linalg.inv(a)
     b = np.dot(X.T, Y)
@@ -21,11 +21,12 @@ def weight(X, Y):
 def predict_y(X, w):
     w = w.T
     y = np.dot(w, (X.T))
+
     return y.T
 
 # return RMSE value
-def rmse(prediction, target):
-    return np.sqrt(((prediction - target) ** 2).mean())
+def rmse(prediction_values, true_values):
+    return np.sqrt(((prediction_values - true_values) ** 2).mean())
 
 # returns identity covariance matrix
 def get_id_matrix(matrix):
@@ -39,14 +40,13 @@ def get_id_matrix(matrix):
 # return ridge weight
 def ridge_weight(X, Y, l):
     a = np.dot(X.T, X)
-
     a2 = l * get_id_matrix(X)
+
     # take inverse
     a = np.linalg.inv(a + a2)
     b = np.dot(X.T, Y)
 
     w = np.dot(a, b)
-
     return w
 
 
@@ -56,35 +56,73 @@ def linear_grad_descent(X, Y):
 
     # set alpha
     alpha = 0.00001
-    w_size = X.shape[1]
 
+    # create random value between 0 and 1 and set to W_t
     rand_val = random.uniform(0,1)
+    W_previous = np.full((X.shape[1], 1), rand_val)
 
-    W_previous = np.full((w_size, 1), rand_val)
-
-    # inner = (X*W_previous)
+    # Calculate W ^ (t+1)
     inner = np.dot(X, W_previous)
     parenthesis = Y - inner
-    # print parenthesis, parenthesis.shape
-
     second = alpha * X.T
-    # print second, second.shape
-
     outer = np.dot(second, parenthesis)
     W_current = W_previous + outer
-    # print W_current
 
     change = 1
     converging_differential = 0.00001
 
+    # loop until change is less than epsilon
     while abs(change) > converging_differential:
+        # reset W_previous
         W_previous = W_current
+
+        # Recalculate W_current
         inner = np.dot(X, W_previous)
         parenthesis = Y - inner
         second = alpha * X.T
         outer = np.dot(second, parenthesis)
         W_current = W_previous + outer
-
         change = np.amin(W_current - W_previous)
 
+    return W_current
+
+
+def ridge_grad_descent(X, Y, l):
+
+    # set alpha
+    alpha  = 0.00001
+
+    # create random value between 0 and 1 and set to W_t
+    rand_val = random.uniform(0,1)
+    W_previous = np.full((X.shape[1], 1), rand_val)
+
+    inner = np.dot(X, W_previous)
+    parenthesis = Y - inner
+    inner_1 = np.dot(X.T, parenthesis)
+    inner_2 = l * W_previous
+
+    big_parenthesis = inner_1 - inner_2
+
+    W_current = W_previous + big_parenthesis
+
+    # print W_current
+    change = 1
+    converging_differential = 0.00001
+
+    while abs(change) > converging_differential:
+        W_previous = W_current
+
+        inner = np.dot(X, W_previous)
+        parenthesis = Y - inner
+        inner_1 = np.dot(X.T, parenthesis)
+        # inner_1 = X.T * parenthesis
+        inner_2 = l * W_previous
+
+        big_parenthesis = inner_1 - inner_2
+        second_term = alpha * big_parenthesis
+
+        W_current = W_previous + second_term
+        change = np.amin(W_current - W_previous)
+
+    # print W_current
     return W_current
--- predict.py
+++ predict.py
@@ -9,6 +9,7 @@ from functions import predict_y
 from functions import rmse
 from functions import ridge_weight
 from functions import linear_grad_descent
+from functions import ridge_grad_descent
 
 import numpy as np
 
@@ -46,11 +47,6 @@ ridge_weigths = []
 
 k_splits = split_kfold(train_x)
 k_splits_y = split_kfold(train_y)
-# print len(k_splits[0])
-# print len(k_splits[1])
-
-# for val in lambda_values:
-#     ridge_weigths.append(ridge_weigth(train_x, train_y, val))
 
 average_errors = []
 
@@ -77,18 +73,17 @@ for lambda_value in lambda_values:
         for j in range(1, len(temp_splits_y)):
             train_y = np.vstack((train_y, temp_splits_y[j]))
 
+        # now do ridge regression stuff
         ridge_w = ridge_weight(train_x, train_y, lambda_value)
         prediction = predict_y(test_x, ridge_w)
 
         error = rmse(prediction, test_y)
         errors.append(error)
-        # print error
-        # now do ridge regression stuff
 
     avg_error = np.mean(errors)
     average_errors.append(avg_error)
 
-# print average_errors
+# find the optimal lambda that generated the smallest RMSE (error) value
 min_index = average_errors.index(min(average_errors))
 min_lambda = lambda_values[min_index]
 
@@ -121,11 +116,99 @@ test_error = rmse(y_test_prediction, test_y)
 
 print 'Training RMSE: ', train_error
 print 'Testing RMSE: ', test_error
-
 print ''
 
 
 # Perform Linear Regression with Gradient Descent
+print 'Calculating (Gradient Descent) Linear Regression... '
 w = linear_grad_descent(train_x, train_y)
 
-print w
+y_train_prediction = predict_y(train_x, w)
+y_test_prediction = predict_y(test_x, w)
+
+train_error = rmse(y_train_prediction, train_y)
+test_error = rmse(y_test_prediction, test_y)
+
+print 'Training RMSE: ', train_error
+print 'Testing RMSE: ', test_error
+print ''
+
+# Perform Ridge Regression with Gradien Descent
+print 'Calculating (Gradient Descent) Ridge Regression.... '
+
+# declare lambda values
+lambda_values = [400, 200, 100, 50, 25, 12.5, 6.25, 3.125, 1.5626, 0.78125]
+ridge_weigths = []
+
+k_splits = split_kfold(train_x)
+k_splits_y = split_kfold(train_y)
+
+average_errors = []
+
+for lambda_value in lambda_values:
+    errors = []
+    print 'Calculating R. Grad Descent with lambda =', lambda_value
+    for i, split in enumerate(k_splits):
+        test_x = k_splits[i]
+        test_y = k_splits_y[i]
+
+        # make a copy of k_splits
+        temp_splits = k_splits[:]
+        temp_splits.pop(i)
+
+        temp_splits_y = k_splits_y[:]
+        temp_splits_y.pop(i)
+
+        train_x = np.array(temp_splits[0])
+        train_y = np.array(temp_splits_y[0])
+
+        # make training array with every other np.array
+        for j in range(1, len(temp_splits)):
+            train_x = np.vstack((train_x, temp_splits[j]))
+
+        for j in range(1, len(temp_splits_y)):
+            train_y = np.vstack((train_y, temp_splits_y[j]))
+
+        # now do ridge regression stuff
+        ridge_w = ridge_grad_descent(train_x, train_y, lambda_value)
+        prediction = predict_y(test_x, ridge_w)
+
+        error = rmse(prediction, test_y)
+        errors.append(error)
+
+    avg_error = np.mean(errors)
+    print 'Average Error is =', avg_error
+    average_errors.append(avg_error)
+
+# find the optimal lambda that generated the smallest RMSE (error) value
+min_index = average_errors.index(min(average_errors))
+min_lambda = lambda_values[min_index]
+
+print ''
+print 'Min-error lambda =', min_lambda
+
+# refresh training values
+train_x = get_feature_values('train.txt')
+train_x = add_ones(train_x)
+
+train_y = get_crime_rates('train.txt')
+
+test_x = get_feature_values('test.txt')
+test_x = add_ones(test_x)
+
+test_y = get_crime_rates('test.txt')
+
+# get ridge gradient descnet weight
+w = ridge_grad_descent(train_x, train_y, min_lambda)
+
+# predict y values
+y_train_prediction = predict_y(train_x, w)
+y_test_prediction = predict_y(test_x, w)
+
+# calculate error
+train_error = rmse(y_train_prediction, train_y)
+test_error = rmse(y_test_prediction, test_y)
+
+print 'Training RMSE: ', train_error
+print 'Testing RMSE: ', test_error
+# print ''
