dc6a2a315d8f1353fbb0a4b454327098e6499cd2
Esai Morales <esaimn96@gmail.com>
2018-02-13 12:02:12 -0800

    ridge grad descent

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 def print_shape(M):
     # print 'Matrix Height: ', M.shape[0]
@@ -49,10 +50,41 @@ def ridge_weight(X, Y, l):
     return w
 
 
-def linear_grad_descent(x, y):
+# Follows formula:
+# W_current = W_previous + alpha*X_transpose (y - X*W_previous)
+def linear_grad_descent(X, Y):
 
     # set alpha
     alpha = 0.00001
-    w_size = x.shape[1]
+    w_size = X.shape[1]
 
-    return 0
+    rand_val = random.uniform(0,1)
+
+    W_previous = np.full((w_size, 1), rand_val)
+
+    # inner = (X*W_previous)
+    inner = np.dot(X, W_previous)
+    parenthesis = Y - inner
+    # print parenthesis, parenthesis.shape
+
+    second = alpha * X.T
+    # print second, second.shape
+
+    outer = np.dot(second, parenthesis)
+    W_current = W_previous + outer
+    # print W_current
+
+    change = 1
+    converging_differential = 0.00001
+
+    while abs(change) > converging_differential:
+        W_previous = W_current
+        inner = np.dot(X, W_previous)
+        parenthesis = Y - inner
+        second = alpha * X.T
+        outer = np.dot(second, parenthesis)
+        W_current = W_previous + outer
+
+        change = np.amin(W_current - W_previous)
+
+    return W_current
